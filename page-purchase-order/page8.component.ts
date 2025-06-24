5f91347f5224f9f3fba09ae5d30e883a6b061f7a
SaiPavan <70737500+SaiPavanNikhil@users.noreply.github.com>
2025-06-24 12:59:02 +0530

    Add files via upload

---
+++ page-purchase-order/page8.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page8',
+  imports: [],
+  templateUrl: './page8.component.html',
+  styleUrl: './page8.component.css'
+})
+export class Page8Component {
+
+}
