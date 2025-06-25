416f4e85c087c15bed85e6b544f9ee2e85798ede
SaiPavan <70737500+SaiPavanNikhil@users.noreply.github.com>
2025-06-25 11:31:55 +0530

    Add files via upload

---
+++ page-po-line-item-bifurcation/page10.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page10',
+  imports: [],
+  templateUrl: './page10.component.html',
+  styleUrl: './page10.component.css'
+})
+export class Page10Component {
+
+}
