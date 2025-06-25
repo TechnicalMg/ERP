8fdf720ef0b7e56fbeeb876bad053e03ba7e9e59
SaiPavan <70737500+SaiPavanNikhil@users.noreply.github.com>
2025-06-25 12:12:25 +0530

    Add files via upload

---
+++ page-view-generate-order/page11.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page11',
+  imports: [],
+  templateUrl: './page11.component.html',
+  styleUrl: './page11.component.css'
+})
+export class Page11Component {
+
+}
