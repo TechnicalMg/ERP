f7e5b07625a3fa3749837a2ceedb31f419bf05c0
SaiPavan <70737500+SaiPavanNikhil@users.noreply.github.com>
2025-06-26 18:50:56 +0530

    Add files via upload

---
+++ page-view-amc/page16.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page16',
+  imports: [CommonModule,HttpClientModule],
+  templateUrl: './page16.component.html',
+  styleUrl: './page16.component.css'
+})
+export class Page16Component {
+
+}
