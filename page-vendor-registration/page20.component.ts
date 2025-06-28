8cf5734a0ada29d629827be5f75356fdb436ea23
SaiPavan <70737500+SaiPavanNikhil@users.noreply.github.com>
2025-06-28 15:28:48 +0530

    Add files via upload

---
+++ page-vendor-registration/page20.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../core/components/header/header.component';
+import { SidebarComponent } from '../../core/components/sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-page20',
+  imports: [HeaderComponent,SidebarComponent],
+  templateUrl: './page20.component.html',
+  styleUrl: './page20.component.css'
+})
+export class Page20Component {
+
+}
