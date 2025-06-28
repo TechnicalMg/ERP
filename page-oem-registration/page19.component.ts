4dfd3cb0ea11caee5e89fd4a82b4c3a2f61bba5f
SaiPavan <70737500+SaiPavanNikhil@users.noreply.github.com>
2025-06-28 14:18:02 +0530

    Add files via upload

---
+++ page-oem-registration/page19.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../core/components/header/header.component';
+import { SidebarComponent } from '../../core/components/sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-page19',
+  imports: [HeaderComponent,SidebarComponent],
+  templateUrl: './page19.component.html',
+  styleUrl: './page19.component.css'
+})
+export class Page19Component {
+
+}
